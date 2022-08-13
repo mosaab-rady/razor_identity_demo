5c77e71c192a7aba64a91c81de394849908adc78
mosaab-rady <70973036+mosaab-rady@users.noreply.github.com>
2022-08-13 23:48:16 +0200

    add class for edit users in role

---
+++ Model/EditUserInRoleViewModel.cs
@@ -0,0 +1,8 @@
+namespace razorIdentityDemo.Models;
+
+public class EditUserInRoleViewModel
+{
+	public string UserId { get; set; }
+	public string Email { get; set; }
+	public bool IsSelected { get; set; }
+}
