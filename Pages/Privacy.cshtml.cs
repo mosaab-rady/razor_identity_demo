53e39954bee5742bad24950d2490696355d5aff0
mosaab-rady <70973036+mosaab-rady@users.noreply.github.com>
2022-08-13 23:51:01 +0200

    add authorize

--- Pages/Privacy.cshtml.cs
+++ Pages/Privacy.cshtml.cs
@@ -1,19 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace razorIdentityDemo.Pages;
 
+[Authorize]
 public class PrivacyModel : PageModel
 {
-    private readonly ILogger<PrivacyModel> _logger;
+	private readonly ILogger<PrivacyModel> _logger;
 
-    public PrivacyModel(ILogger<PrivacyModel> logger)
-    {
-        _logger = logger;
-    }
+	public PrivacyModel(ILogger<PrivacyModel> logger)
+	{
+		_logger = logger;
+	}
 
-    public void OnGet()
-    {
-    }
+	public void OnGet()
+	{
+	}
 }
 
