533476cf7751594be8721071a275b9b87415b113
mosaab-rady <70973036+mosaab-rady@users.noreply.github.com>
2022-08-13 23:47:37 +0200

    add class for edit role view

---
+++ Model/EditRoleViewModel.cs
@@ -0,0 +1,12 @@
+namespace razorIdentityDemo.Models;
+
+public class EditRoleViewModel
+{
+	public EditRoleViewModel()
+	{
+		Users = new List<string>();
+	}
+	public string Id { get; set; }
+	public string RoleName { get; set; }
+	public List<string> Users { get; set; }
+}
